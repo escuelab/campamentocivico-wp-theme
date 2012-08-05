449e6af4502d7b1df6b81d7fd1f5697eb91aa082
Mariano Crowe <merunga@gmail.com>
2012-08-05 10:35:53 -0500

    colores y fuentes

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+<hr />
+</div>
+<div id="footer">
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		<?php bloginfo('name'); ?> is powered by <a href="http://wordpress.org/">WordPress</a> | Theme By <a href="http://www.theme-generator.net/wordpress" target="_blank" title="Wordpress Theme Generator"/>Wordpress Theme Generator</a><br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>.
+		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</p>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
